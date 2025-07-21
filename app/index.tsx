9c155c0e98b5e81d9b399ae419eb5d9e09879795
agstiana <105841104722@student.unismuh.ac.id>
2025-07-21 21:35:54 +0800

    revisi tugas 4

--- app/index.tsx
+++ app/index.tsx
@@ -1,56 +1,54 @@
 import { ScrollView, Text } from "react-native";
 
+// PATOKAN NIM: 105841104722 (AGUSTIANA)
+
 export default function Index() {
   return (
-    
     <ScrollView contentContainerStyle={{ padding: 20 }}>
-      
       <Text style={{ fontFamily: "Rubik-Italic-VariableFont_wght", fontSize: 26, color: "blue", fontWeight: "bold" }}>
         SELFIRA AYU SAFITRI
-        {"\n"}105841104122
+        {"\n"}105841104222
       </Text>
       <Text style={{ fontFamily: "lato-bold", fontSize: 26, color: "blue", fontWeight: "bold" }}>
         ALIF RYANTO RAHMAN
-        {"\n"}105841104222
+        {"\n"}105841104322
       </Text>
       <Text style={{ fontFamily: "OpenSans_Condensed-Bold", fontSize: 24 }}>
         ERIKA YANTI
-        {"\n"}105841104322
+        {"\n"}105841104422
       </Text>
       <Text style={{ fontFamily: "poppins-bold", fontSize: 24 }}>
         FIFIANA
-        {"\n"}105841104422
+        {"\n"}105841104522
       </Text>
       <Text style={{ fontFamily: "spacemono-regular", fontSize: 24 }}>
         MUH AKBAR HAERUDDIN
-        {"\n"}105841104522
+        {"\n"}105841104622
       </Text>
       <Text style={{ fontFamily: "Roboto_Condensed-Bold", fontSize: 24 }}>
         AGUSTIANA
-        {"\n"}105841104622
+        {"\n"}105841104722
       </Text>
       <Text style={{ fontFamily: "ibmplex-variable", fontSize: 24 }}>
-        105841104722
+        105841104822
         {"\n"}Dia Ramatilla
       </Text>
       <Text style={{ fontFamily: "inter-variable", fontSize: 24 }}>
-        105841104822
+        105841104922
         {"\n"}juliani
       </Text>
       <Text style={{ fontFamily: "montserrat-variable", fontSize: 24 }}>
-        105841104922
+        105841105022
         {"\n"}Azzah Aulia
       </Text>
       <Text style={{ fontFamily: "raleway-variable", fontSize: 24 }}>
-        105841105022
+        105841105122
         {"\n"}Syauqiyah Mujahidah Yahya
       </Text>
       <Text style={{ fontFamily: "sora-variable", fontSize: 24 }}>
-        105841105122
+        105841105222
         {"\n"}Mar'atul Aziza
       </Text>
-
     </ScrollView>
-    
   );
-  }
+}
