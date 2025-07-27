08f4a8d2c4e1480a73cea350a1c1cae79939fbcd
agstiana <105841104722@student.unismuh.ac.id>
2025-07-27 22:04:42 +0800

    selesai tugas 6

--- app/index.tsx
+++ app/index.tsx
@@ -1,54 +1,28 @@
-import { ScrollView, Text } from "react-native";
-
-// PATOKAN NIM: 105841104722 (AGUSTIANA)
+import React from 'react';
+import { View, ScrollView, Text } from 'react-native';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import Feather from '@expo/vector-icons/Feather';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function Index() {
   return (
-    <ScrollView contentContainerStyle={{ padding: 20 }}>
-      <Text style={{ fontFamily: "Rubik-Italic-VariableFont_wght", fontSize: 26, color: "blue", fontWeight: "bold" }}>
-        SELFIRA AYU SAFITRI
-        {"\n"}105841104222
-      </Text>
-      <Text style={{ fontFamily: "lato-bold", fontSize: 26, color: "blue", fontWeight: "bold" }}>
-        ALIF RYANTO RAHMAN
-        {"\n"}105841104322
-      </Text>
-      <Text style={{ fontFamily: "OpenSans_Condensed-Bold", fontSize: 24 }}>
-        ERIKA YANTI
-        {"\n"}105841104422
-      </Text>
-      <Text style={{ fontFamily: "poppins-bold", fontSize: 24 }}>
-        FIFIANA
-        {"\n"}105841104522
-      </Text>
-      <Text style={{ fontFamily: "spacemono-regular", fontSize: 24 }}>
-        MUH AKBAR HAERUDDIN
-        {"\n"}105841104622
-      </Text>
-      <Text style={{ fontFamily: "Roboto_Condensed-Bold", fontSize: 24 }}>
-        AGUSTIANA
-        {"\n"}105841104722
-      </Text>
-      <Text style={{ fontFamily: "ibmplex-variable", fontSize: 24 }}>
-        105841104822
-        {"\n"}Dia Ramatilla
-      </Text>
-      <Text style={{ fontFamily: "inter-variable", fontSize: 24 }}>
-        105841104922
-        {"\n"}juliani
-      </Text>
-      <Text style={{ fontFamily: "montserrat-variable", fontSize: 24 }}>
-        105841105022
-        {"\n"}Azzah Aulia
-      </Text>
-      <Text style={{ fontFamily: "raleway-variable", fontSize: 24 }}>
-        105841105122
-        {"\n"}Syauqiyah Mujahidah Yahya
-      </Text>
-      <Text style={{ fontFamily: "sora-variable", fontSize: 24 }}>
-        105841105222
-        {"\n"}Mar'atul Aziza
-      </Text>
+    <ScrollView contentContainerStyle={{ padding: 20, flexGrow: 1 }}>
+      <Text style={{ fontSize: 20, marginBottom: 10 }}>Daftar 10 Ikon:</Text>
+
+      <View style={{ flexDirection: "row", flexWrap: "wrap", justifyContent: "space-between" }}>
+        <AntDesign name="stepforward" size={30} color="black" />
+        <AntDesign name="stepbackward" size={30} color="black" />
+        <Entypo name="500px" size={30} color="black" />
+        <Entypo name="aircraft" size={30} color="black" />
+        <EvilIcons name="bell" size={30} color="black" />
+        <EvilIcons name="calendar" size={30} color="black" />
+        <Feather name="activity" size={30} color="black" />
+        <Feather name="airplay" size={30} color="black" />
+        <FontAwesome name="heart" size={30} color="black" />
+        <FontAwesome name="star" size={30} color="black" />
+      </View>
     </ScrollView>
   );
 }
