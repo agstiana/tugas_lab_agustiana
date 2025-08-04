7b9245ec07dbcfec4b628a8f174daccae34e46b4
agstiana <105841104722@student.unismuh.ac.id>
2025-08-04 16:15:48 +0800

    selesai tugas 8

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,39 +2,20 @@ import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import { TouchableOpacity, View } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
-// Mencegah SplashScreen hilang sebelum font termuat
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    // 5 FONT STATIS
-    "lato-bold": require("../assets/fonts/Lato-Bold.ttf"),
-    "OpenSans_Condensed-Bold": require("../assets/fonts/OpenSans_Condensed-Bold.ttf"),
-    "poppins-bold": require("../assets/fonts/Poppins-Bold.ttf"),
-    "spacemono-regular": require("../assets/fonts/SpaceMono-Regular.ttf"),
-    "Roboto_Condensed-Bold": require("../assets/fonts/Roboto_Condensed-Bold.ttf"),
-
-    // 5 FONT VARIABLE
-    "ibmplex-variable": require("../assets/fonts/IBMPlexSans-Italic-VariableFont_wdth,wght.ttf"),
-    "inter-variable": require("../assets/fonts/Inter-VariableFont_opsz,wght.ttf"),
-    "montserrat-variable": require("../assets/fonts/Montserrat-Italic-VariableFont_wght.ttf"),
-    "raleway-variable": require("../assets/fonts/Raleway-Italic-VariableFont_wght.ttf"),
-    "sora-variable": require("../assets/fonts/Sora-VariableFont_wght.ttf"),
-
-    // Tambahan agar Frasydi mendeteksi Rubik
-    "Rubik-Italic-VariableFont_wght": require("../assets/fonts/Rubik-Italic-VariableFont_wght.ttf"),
-  });
-
-  useEffect(() => {
-    if (loaded && !error) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded, error]);
-
-  if (!loaded && !error) {
-    return null; // putih saat font belum termuat
-  }
-
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
 }
